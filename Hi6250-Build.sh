99806c83e76b3f462dccaeb50fb48103b173b736
Ramy ahmed <ramy@ahmedramy.com>
2018-10-05 20:27:48 +0200

    Update Hi6250-Build.sh

--- Hi6250-Build.sh
+++ Hi6250-Build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd $(romname)
+if [ "$romname" = "PixysOS" ];then
 . build/envsetup.sh
-lunch aosp_hi6250-userdebug
+lunch lineage_beryllium-userdebug
+make pixys 2>&1 | tee build-log.txt
+else [ "$AndroidVer" = "Pie" ];then
+. build/envsetup.sh
+lunch lineage_beryllium-userdebug
 mka bacon 2>&1 | tee build-log.txt
