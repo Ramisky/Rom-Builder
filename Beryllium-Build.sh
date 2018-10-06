be180b817eccbf3468d2d44ff63e8a3ee882c3ff
Ramy ahmed <ramy@ahmedramy.com>
2018-10-06 12:43:13 +0200

    Update Beryllium-Build.sh

--- Beryllium-Build.sh
+++ Beryllium-Build.sh
@@ -4,7 +4,7 @@ if [ "$romname" = "PixysOS" ];then
 . build/envsetup.sh
 lunch lineage_beryllium-userdebug
 make pixys 2>&1 | tee build-log.txt
-else [ "$AndroidVer" = "Pie" ];then
+else
 . build/envsetup.sh
 lunch lineage_beryllium-userdebug
 mka bacon 2>&1 | tee build-log.txt
