0334b2b9a4c2c9e2b4d77d2db9dafebc427fca8b
Ramy ahmed <ramy@ahmedramy.com>
2018-10-06 10:09:07 +0200

    Update Beryllium-Sync.sh

--- Beryllium-Sync.sh
+++ Beryllium-Sync.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 elif [ "$romname" = "CypherOS" ];then
-git clone https://github.com/Ramyski/Device_Xiaomi_Beryllium -b Lineage-16 ${romname}/device/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_device_xiaomi_beryllium -b Lineage-16 ${romname}/device/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_vendor_xiaomi_sdm845-common ${romname}/vendor/xiaomi/sdm845-common
+git clone https://github.com/Ramyski/android_vendor_xiaomi_msm8998-common ${romname}/vendor/xiaomi/msm8998-common
+git clone https://github.com/Ramyski/android_vendor_xiaomi_Beryllium ${romname}/vendor/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_device_xiaomi_sdm845-common ${romname}/device/xiaomi/sdm845-common
+git clone https://github.com/Ramyski/android_kernel_xiaomi_sdm845 ${romname}/kernel/xiaomi/sdm845
 elif [ "$romname" = "Lineage" ];then
-git clone https://github.com/Ramyski/Device_Xiaomi_Beryllium -b Cypher-9.0 ${romname}/device/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_device_xiaomi_beryllium -b Cypher-9.0 ${romname}/device/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_vendor_xiaomi_sdm845-common ${romname}/vendor/xiaomi/sdm845-common
+git clone https://github.com/Ramyski/android_vendor_xiaomi_msm8998-common ${romname}/vendor/xiaomi/msm8998-common
+git clone https://github.com/Ramyski/android_vendor_xiaomi_Beryllium ${romname}/vendor/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_device_xiaomi_sdm845-common ${romname}/device/xiaomi/sdm845-common
+git clone https://github.com/Ramyski/android_kernel_xiaomi_sdm845 ${romname}/kernel/xiaomi/sdm845
 else
-git clone https://github.com/Ramyski/Device_Xiaomi_Beryllium -b AOSP ${romname}/device/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_device_xiaomi_beryllium -b AOSP ${romname}/device/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_vendor_xiaomi_sdm845-common ${romname}/vendor/xiaomi/sdm845-common
+git clone https://github.com/Ramyski/android_vendor_xiaomi_msm8998-common ${romname}/vendor/xiaomi/msm8998-common
+git clone https://github.com/Ramyski/android_vendor_xiaomi_Beryllium ${romname}/vendor/xiaomi/beryllium
+git clone https://github.com/Ramyski/android_device_xiaomi_sdm845-common ${romname}/device/xiaomi/sdm845-common
+git clone https://github.com/Ramyski/android_kernel_xiaomi_sdm845 ${romname}/kernel/xiaomi/sdm845
 fi
-git clone https://github.com/Ramyski/Vendor_Xiaomi_SDM845-Common ${romname}/vendor/xiaomi/sdm845-common
-git clone https://github.com/Ramyski/Vendor_Xiaomi_MSM8998-Common ${romname}/vendor/xiaomi/msm8998-common
-git clone https://github.com/Ramyski/Vendor_Xiaomi_Beryllium ${romname}/vendor/xiaomi/beryllium
-git clone https://github.com/Ramyski/Device_Xiaomi_SDM845-Common ${romname}/device/xiaomi/sdm845-common
-git clone https://github.com/Ramyski/Kernel_Xiaomi_SDM845 ${romname}/kernel/xiaomi/sdm845
